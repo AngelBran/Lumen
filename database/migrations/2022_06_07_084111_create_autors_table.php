fbad1752b530b631519ff47f076c8b002df6ddb1
Angel Bran <angelbranm@gmail.com>
2022-06-07 09:57:18 -0600

    Initial

---
+++ database/migrations/2022_06_07_084111_create_autors_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAutorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('autores', function (Blueprint $table) {
+            $table->id('id');
+            $table->string('nombre',150);
+            $table->integer('estado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('autores');
+    }
+}
